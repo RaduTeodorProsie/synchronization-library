0c13b8da0f8c0914e72fbc23e86af06881700bb1
RaduTeodorProsie <psn.radu@gmail.com>
2025-12-25 12:15:24 +0200

    refactor rwlock

--- src/RwLock.cpp
+++ src/RwLock.cpp
@@ -1,3 +1,29 @@
-//
-// Created by Radu Prosie on 12/24/2025.
-//
+#include "RwLock.h"
+
+void RwLock::lockRead() {
+  std::scoped_lock lock(revolvingDoor, counter);
+  if (activeReaders == 0) {
+    data.lock();
+  }
+
+  activeReaders++;
+}
+
+void RwLock::unlockRead() {
+  LockGuard<std::mutex> lock(counter);
+  if (activeReaders == 1) {
+    data.unlock();
+  }
+
+  activeReaders--;
+}
+
+void RwLock::lockWrite() {
+  revolvingDoor.lock();
+  data.lock();
+}
+
+void RwLock::unlockWrite() {
+  data.unlock();
+  revolvingDoor.unlock();
+}
--- src/RwLock.h
+++ src/RwLock.h
@@ -1,7 +1,9 @@
 #ifndef RWLOCK_H
 #define RWLOCK_H
+#include "LockGuard.h"
 #include <condition_variable>
 #include <mutex>
+
 
 class RwLock {
   std::mutex revolvingDoor;
@@ -10,35 +12,10 @@ class RwLock {
   size_t activeReaders = 0;
 
 public:
-  void lockRead() {
-    std::scoped_lock lock(revolvingDoor, counter);
-    if (activeReaders == 0) {
-      data.lock();
-    }
-
-    activeReaders++;
-  }
-
-#include "LockGuard.h"
-
-  void unlockRead() {
-    LockGuard<std::mutex> lock(counter);
-    if (activeReaders == 1) {
-      data.unlock();
-    }
-
-    activeReaders--;
-  }
-
-  void lockWrite() {
-    revolvingDoor.lock();
-    data.lock();
-  }
-
-  void unlockWrite() {
-    data.unlock();
-    revolvingDoor.unlock();
-  }
+  void lockRead();
+  void unlockRead();
+  void lockWrite();
+  void unlockWrite();
 };
 
 #endif // RWLOCK_H
